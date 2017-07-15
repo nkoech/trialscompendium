e046c539e04376c07c02506323ddc62d4fe3117d
Koech Nicholas <koechnicholas@gmail.com>
2017-07-15 23:35:20 +0300

    Started an app "Trials"

--- config/settings/common.py
+++ config/settings/common.py
@@ -49,7 +49,7 @@ THIRD_PARTY_APPS = [
 
 # Apps specific for this project go here.
 LOCAL_APPS = [
-    # 'trialscompendium.countries',
+    'trialscompendium.trials',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^api/v1/trials', include('trialscompendium.trials.api.urls', namespace='plot_api'))
 ]
 
 if settings.DEBUG:

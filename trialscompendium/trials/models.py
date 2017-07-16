3a304b8cc74d84f8662e4bc7efacdca3c8c828e9
Koech Nicholas <koechnicholas@gmail.com>
2017-07-17 01:31:14 +0300

    Added "Treatment", "Plot" and "TrialYield"

--- trialscompendium/trials/models.py
+++ trialscompendium/trials/models.py
@@ -11,6 +11,8 @@ from trialscompendium.utils.modelmanagers import (
     model_foreign_key_qs,
     model_type_filter,
     create_model_type,
+    get_year_choices,
+    get_datetime_now,
 )
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
@@ -37,31 +39,33 @@ class Treatment(AuthUserDetail, CreateUpdateTime):
         ('P60', 'P60'),
     )
 
-    TILLAGE_PRACTICE = (
+    TILLAGE_PRACTICES = (
         ('Conservation', 'Conservation'),
         ('Conventional', 'Conventional'),
         ('Reduced', 'Reduced'),
         ('Intensive', 'Intensive'),
     )
 
-    CROPPING_SYSTEM = (
+    CROPPING_SYSTEMS = (
         ('Continuous', 'Continuous'),
         ('Intercropping', 'Intercropping'),
         ('Rotation', 'Rotation'),
     )
 
-    slug = models.SlugField(max_length=50, unique=True, blank=True)
     no_replicate = models.PositiveSmallIntegerField(verbose_name='Number of replication')
     nitrogen_treatment = models.DecimalField(max_length=4, choices=NITROGEN_TREATMENT, default=NITROGEN_TREATMENT[0][0])
     phosphate_treatment = models.CharField(max_length=4, choices=PHOSPHATE_TREATMENT, default=PHOSPHATE_TREATMENT[0][0])
-    tillage_practice = models.CharField(max_length=30, choices=TILLAGE_PRACTICE, default=TILLAGE_PRACTICE[0][0])
-    cropping_system = models.CharField(max_length=30, choices=CROPPING_SYSTEM, default=CROPPING_SYSTEM[0][0])
+    tillage_practice = models.CharField(max_length=30, choices=TILLAGE_PRACTICES, default=TILLAGE_PRACTICES[0][0])
+    cropping_system = models.CharField(max_length=30, choices=CROPPING_SYSTEMS, default=CROPPING_SYSTEMS[0][0])
     crops_grown = models.CharField(max_length=60)
     farm_yard_manure = models.BooleanField(default=True)
     farm_residue = models.BooleanField(default=True)
 
     def __unicode__(self):
-        str_format = '{0}: {1}{2}'.format(self.crops_grown, self.nitrogen_treatment, self.phosphate_treatment)
+        str_format = 'TP: {0}, CS: {1}, {2}, {3}{4}, Rep: {5}, FYM: {6}, Residue: {7}'.format(
+            self.tillage_practice, self.cropping_system, self.crops_grown, self.nitrogen_treatment,
+            self.phosphate_treatment, self.no_replicate, self.farm_yard_manure, self.farm_residue
+        )
         return str_format
 
     def __str__(self):
@@ -77,8 +81,12 @@ class Treatment(AuthUserDetail, CreateUpdateTime):
         return reverse('trials_api:treatment_detail', kwargs={'pk': self.pk})
 
     class Meta:
+        unique_together = [
+            'no_replicate', 'nitrogen_treatment', 'phosphate_treatment', 'tillage_practice',
+            'cropping_system', 'crops_grown', 'farm_yard_manure', 'farm_residue'
+        ]
         ordering = ['-time_created', '-last_update']
-        verbose_name_plural = 'Subpillars'
+        verbose_name_plural = 'Treatments'
 
     @property
     def plot_relation(self):
@@ -90,3 +98,128 @@ class Treatment(AuthUserDetail, CreateUpdateTime):
         instance = self
         qs = Plot.objects.filter_by_model_type(instance)
         return qs
+
+
+class PlotManager(models.Manager):
+    """
+    Plot model manager
+    """
+    def filter_by_model_type(self, instance):
+        """
+        Query related objects/model type
+        :param instance: Object instance
+        :return: Matching object else none
+        :rtype: Object/record
+        """
+        obj_qs = model_foreign_key_qs(instance, self, PlotManager)
+        if obj_qs.exists():
+            return model_type_filter(self, obj_qs, PlotManager)
+
+
+class Plot(AuthUserDetail, CreateUpdateTime):
+    """
+    Plot model. Creates plot entity.
+    """
+    slug = models.SlugField(max_length=120, unique=True, blank=True)
+    treatment = models.ForeignKey(Treatment, on_delete=models.PROTECT)
+    trial_id = models.CharField(max_length=20, blank=True, null=True)
+    plot_id = models.CharField(max_length=20, unique=True)
+    sub_plot_id = models.CharField(max_length=15, blank=True, null=True)
+    objects = PlotManager()
+
+    def __unicode__(self):
+        return self.plot_id
+
+    def __str__(self):
+        return self.plot_id
+
+    def get_api_url(self):
+        """
+        Get plot URL as a reverse from model
+        :return: URL
+        :rtype: String
+        """
+        return reverse('trials_api:plot_detail', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['-time_created', '-last_update']
+        verbose_name_plural = 'Plots'
+
+    @property
+    def trial_yield_relation(self):
+        """
+        Get related trial yield
+        :return: Query result from the trial yield model
+        :rtype: object/record
+        """
+        instance = self
+        qs = TrialYield.objects.filter_by_model_type(instance)
+        return qs
+
+
+@receiver(pre_save, sender=Plot)
+def pre_save_plot_receiver(sender, instance, *args, **kwargs):
+    """
+    Create a slug before save.
+    :param sender: Signal sending object
+    :param instance: Object instance
+    :param args: Any other argument
+    :param kwargs: Keyword arguments
+    :return: None
+    :rtype: None
+    """
+    if not instance.slug:
+        instance.slug = create_slug(instance, Plot, instance.plot_id)
+
+
+class TrialYieldManager(models.Manager):
+    """
+    Trial yield model manager
+    """
+    def filter_by_model_type(self, instance):
+        """
+        Query related objects/model type
+        :param instance: Object instance
+        :return: Matching object else none
+        :rtype: Object/record
+        """
+        obj_qs = model_foreign_key_qs(instance, self, TrialYieldManager)
+        if obj_qs.exists():
+            return model_type_filter(self, obj_qs, TrialYieldManager)
+
+
+class TrialYield(AuthUserDetail, CreateUpdateTime):
+    """
+    Trial yield model. Creates trial yield entity
+    """
+    SEASONS = (
+        ('Short Rains', 'Short Rains'),
+        ('Long Rains', 'Long Rains'),
+    )
+
+    plot = models.ForeignKey(Plot, on_delete=models.PROTECT)
+    observation = models.CharField(max_length=45)
+    year = models.SmallIntegerField(choices=get_year_choices(), default=get_datetime_now())
+    season = models.CharField(max_length=11, choices=SEASONS, unique=True)
+    trial_yield = models.DecimalField(max_digits=6, decimal_places=2)
+    objects = TrialYieldManager()
+
+    def __unicode__(self):
+        str_format = '{0}, {1}, {2}, {3}'.format(self.observation, self.year, self.season, self.trial_yield)
+        return str(str_format)
+
+    def __str__(self):
+        str_format = '{0}, {1}, {2}, {3}'.format(self.observation, self.year, self.season, self.trial_yield)
+        return str(str_format)
+
+    def get_api_url(self):
+        """
+        Get trial yield URL as a reverse from model
+        :return: URL
+        :rtype: String
+        """
+        return reverse('trials_api:trial_yield_detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-time_created', '-last_update']
+        verbose_name_plural = 'Trial Yields'

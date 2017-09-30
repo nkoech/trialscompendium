730d78422d1f1101cdcfcdeaeb7d80204cf690e4
Koech Nicholas <koechnicholas@gmail.com>
2017-09-30 07:50:37 +0300

    added anugularjs "ui-select"

--- trialscompendium/frontend/src/app/core/core.module.js
+++ trialscompendium/frontend/src/app/core/core.module.js
@@ -3,13 +3,15 @@ require('angular-route');
 require('angular-resource');
 require('angular-sanitize');
 require('angular-ui-bootstrap');
+require('ui-select');
 
 angular.module('app.core', [
     'ngAnimate',
     'ngRoute',
     'ngResource',
     'ngSanitize',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'ui.select'
 ]);
 
 require('./trial.fct');

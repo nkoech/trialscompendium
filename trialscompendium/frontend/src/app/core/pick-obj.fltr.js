a6a41f26acf26efcc99ad0e47845a662e26119c1
Koech Nicholas <koechnicholas@gmail.com>
2017-10-20 19:23:23 +0300

    Changed "prop" to "item"

--- trialscompendium/frontend/src/app/core/pick-obj.fltr.js
+++ trialscompendium/frontend/src/app/core/pick-obj.fltr.js
@@ -15,12 +15,12 @@ function pickObj() {
     function pickObjFilter(input, filterProp) {
         angular.forEach(input, function (value, key) {
             var obj = {};
-            if (value !== undefined && value !== null){
+            if (value !== undefined || value !== null){
                 if (typeof value === 'object'){
                     pickObjFilter(value, filterProp);
                 }else{
-                    angular.forEach(filterProp, function (prop) {
-                        if (prop === key) {
+                    angular.forEach(filterProp, function (item) {
+                        if (item === key) {
                             // Replace true and false js values with Plus and Minus string respectively
                             value = (value === true) ? 'Plus' : (value === false) ? 'Minus' : value;
                             obj[key] = value;

4e4b2ef125fc1131263fd36968e41bcbf50286ea
Koech Nicholas <koechnicholas@gmail.com>
2017-11-13 02:16:27 +0300

    refactored "pickMultiObj" filter

--- trialscompendium/frontend/src/app/core/pick-multi-obj.fltr.js
+++ trialscompendium/frontend/src/app/core/pick-multi-obj.fltr.js
@@ -12,50 +12,18 @@ angular
 pickMultiObj.$inject = ['valReplaceFilter', 'parseNumFilter'];
 
 function pickMultiObj(valReplaceFilter, parseNumFilter) {
-    // return pickMultiObjFilter;
-    // function pickMultiObjFilter(input, filterProp, replaceValue) {
-    //     if ((!angular.isArray(input) && (input === undefined || input === null)) || (filterProp === undefined || filterProp === null)) {return input;}
-    //     // var outObjArr = input.map(function (obj) {
-    //     //     var outObj = {};
-    //     //     angular.forEach(filterProp, function (key) {
-    //     //         if (obj.hasOwnProperty(key)){
-    //     //             var value = valReplaceFilter(obj[key], replaceValue);
-    //     //             outObj[key] = parseNumFilter(value);
-    //     //         }
-    //     //     });
-    //     //     return outObj;
-    //     // });
-    //     // return outObjArr;
-    //
-    //     return input.map(function(obj){
-    //         return Object.keys(obj).filter(function(key){
-    //             return filterProp.includes(key);
-    //         }).reduce(function(newObj, key){
-    //             var value = valReplaceFilter(obj[key], replaceValue);
-    //             newObj[key] = parseNumFilter(value);
-    //             return newObj;
-    //         }, {});
-    //     });
-    // }
-
     return pickMultiObjFilter;
     function pickMultiObjFilter(input, filterProp, replaceValue) {
-        var outObjArr = [];
         if ((!angular.isArray(input) && (input === undefined || input === null)) || (filterProp === undefined || filterProp === null)) {return input;}
-        angular.forEach(input, function(obj){
-            var outObj = {};
-            angular.forEach(obj, function (value, key) {
-                value = valReplaceFilter(value, replaceValue);
-                value = parseNumFilter(value);
-                if (angular.isArray(filterProp) && filterProp.length){
-                    angular.forEach(filterProp, function (item) {item === key ? outObj[key] = value : value;});
-                }else{
-                    filterProp === key ? outObj[key] = value : value;
-                }
-            });
-            outObjArr = outObjArr.concat(outObj);
+        return input.map(function(obj){
+            return Object.keys(obj).filter(function(key){
+                return filterProp.includes(key);
+            }).reduce(function(newObj, key){
+                var value = valReplaceFilter(obj[key], replaceValue);
+                newObj[key] = parseNumFilter(value);
+                return newObj;
+            }, {});
         });
-        return outObjArr;
     }
 }
 

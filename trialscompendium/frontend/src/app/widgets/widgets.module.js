0687ec265d18c5cd11f1f0d265a0e7e9b5128614
Koech Nicholas <koechnicholas@gmail.com>
2018-02-18 21:15:07 +0300

    Added content to about section

--- trialscompendium/frontend/src/app/widgets/widgets.module.js
+++ trialscompendium/frontend/src/app/widgets/widgets.module.js
@@ -1,5 +1,4 @@
 angular.module('app.widgets', []);
 
 require('./search-trial/search-trial.drct');
-require('./background-image/background-image.drct');
 // require('./on-finish-repeat/on-finish-repeat.drct');
